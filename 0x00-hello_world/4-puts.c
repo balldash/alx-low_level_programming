f094cb0612be4383a25b40eea3331faa9455e4ac
Bislon Zulu <bislonzulu1@outlook.com>
2022-06-30 21:54:26 +0200

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main-"The main function"
+ *Return: 0 on sucess
+ **/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
