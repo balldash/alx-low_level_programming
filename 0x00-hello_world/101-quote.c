fffadc170b0559ef139b5c38a1f04e7005b81878
Bislon Zulu <bislonzulu1@outlook.com>
2022-06-30 22:36:53 +0200

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-"The main function"
+ * Return: 1 (error)
+ **/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
