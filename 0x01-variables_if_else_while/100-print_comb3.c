28141b04d7ab6d30391b8010cf86461fc1763b7f
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-02 02:14:35 +0200

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d < 100; d++)
+	for (d = 1; d < 90; d++)
 	{
 		putchar((d / 10) + '0');
 		putchar((d % 10) + '0');
-		if (d != 99)
+		if (d != 89)
 		{
 			putchar(',');
 			putchar(' ');
