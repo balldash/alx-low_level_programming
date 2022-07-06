a176beb413337cabcbcc34b06e6f63c71c5a1b72
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-06 02:50:44 +0200

    task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	long int fibonacci[5000];
+	long int sum = 0;
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+
+	for (i = 2; i < 5000; i++)
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+	
+	int j;
+
+	for (j = 0; j < 5000; j++)
+	{
+		if (fibonacci[j] > 4000000)
+			break;
+		if (fibonacci[j] % 2 == 0)
+			sum = sum + fibonacci[j];
+	};
+	printf("%ld\n", sum);
+}
