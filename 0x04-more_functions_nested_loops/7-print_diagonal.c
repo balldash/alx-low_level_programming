25a3f788f698a7587a32c6185a35be0f2b2246b3
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-07 22:57:07 +0200

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,18 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			_putchar(' ');
+			for (j = 1; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar(92);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
