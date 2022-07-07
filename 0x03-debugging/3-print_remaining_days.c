521fe5409b332d0f73f57976403b7d569093f53f
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-07 17:38:27 +0200

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,13 +14,11 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
 	{
-		printf("Day of the year: %d\n", day);
-
 		if (month >= 2)
 		{
 			day++;
 		}
-
+		printf("Day of the year: $d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
