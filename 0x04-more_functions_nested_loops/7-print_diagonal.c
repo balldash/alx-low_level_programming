242a0541d28ac704c5cc87abbad578d58bef8417
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-07 20:04:05 +0200

    task 6

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,8 +2,19 @@
 
 /**
  * print_diagonal - draws a diagonal line on the terminal
+ * @n: the number of times to draw the diagonal line
  * Return: nothing (void)
  */
 void print_diagonal(int n)
 {
+	int i;
 
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('\\');
+		}
+	}
+	_putchar('\n');
+}
