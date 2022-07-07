d594afb229f97f4143482d20d64d80dbb5570fb6
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-07 19:36:52 +0200

    task 5

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,10 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i <= n; i++)
+	if (n > 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar('_');
+		for (i = 0; i < n; i++)
+			_putchar('_');
 	}
 	_putchar('\n');
 }
