ab94e4c2ca36761567a4f35723b7638abb3c85dd
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-07 19:34:46 +0200

    task 5

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line = draws a straight line in the terminal
+ * Return: nothing (void)
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
