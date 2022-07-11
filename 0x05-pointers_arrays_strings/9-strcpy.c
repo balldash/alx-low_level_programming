894850630da202cfae7e108037aefeacc306f5c7
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-12 01:46:06 +0200

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *_strcopy - copies the string pointed to by src
+ * including the terminating null bype
+ * to the buffer pointed to by dest
+ * Return: the pointer to dest
+ */
+char *_strcopy(char *dest, char *src)
+{
+	char *holder = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (holder);
