2bb64f0df46f8bc36f21c6b48f8e23d002025b24
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-12 01:17:14 +0200

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string,
+ * followed by a new line
+ * @str: the string input
+ */
+void puts_half(char *str)
+{
+	len = 0, n;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+		n = len /2;
+	else
+		n = (len + 1) / 2;
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
