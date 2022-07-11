e0b8b3d43cc1accda6927b7503d49101e0dfaeeb
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-12 01:01:56 +0200

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line.
+ * @s: the string input
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+
+	_putchar('\n');
+}
