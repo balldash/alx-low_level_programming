f10e3fc43f6acc33cb3a0cb80dc0a2d94ef9208f
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-12 00:32:43 +0200

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int and updates the value it points to to 98
+ * reset_to_98 - takes a pointer to an int
+ * and updates the value it points to to 98
  * @n: the pointer to update
  * Return: Nothing
  */
