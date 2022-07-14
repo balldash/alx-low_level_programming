fb8f8a55583c137c21a48659fb9b3e7020a8e061
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-14 22:09:42 +0200

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,21 +9,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *holder = dest, *start = src;
+	int i;
 
-	while (*src)
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
 	{
-		srclen++;
-		src++;
+		dest[i] = '\0';
+		i++;
 	}
-	srclen++;
 
-	if (n > srclen)
-		n = srclen;
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-	return (holder);
+	return (dest);
 }
