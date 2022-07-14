a35ed3402f378cf7c3b907ccebdcebc6a2eb758b
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-14 22:14:23 +0200

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,20 +2,20 @@
 
 /**
  * _strcmp - a function that compares two strings
- * s1: the first string to compare
- * s2: the second string to compare
+ * @s1: the first string to compare
+ * @s2: the second string to compare
  * Return: the difference of s1 - s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
+	int i = 0;
 
-	return (*s1 - *s2);
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+		i++;
+
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
+	else
+		return (0);
 }
