00d069a52d14660c6a96273897ecda9d91b61074
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-15 14:59:18 +0200

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,20 @@
 
 char *leet(char *s)
 {
-	int = 0, j;
-	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
-	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int i, j;
 
-	while (s[i])
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
-			if (s[i] == str[j])
-				s[i] = subs[j];
-		i++;
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
 	}
 
 	return (s);
