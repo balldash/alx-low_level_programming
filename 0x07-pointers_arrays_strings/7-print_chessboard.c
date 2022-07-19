21a1b6842b3d78cdf7e2baa8aa5c6a4cd6f7136f
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-19 22:46:42 +0200

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard set given 2d array
+ * @a: 2d array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+			_putchar(a[row][column]);
+		_puthcar('\n');
+	}
+}
