5655f541024de53af8e2c9d0a7589a7f87bc6230
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-28 21:50:44 +0200

    task 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,7 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size > new_size)
 		old_size = new_size;
 	for (i = 0; i < old_size; i++)
-		new_location[i] = new+ptr[i];
+		new_location[i] = new_ptr[i];
 	free(ptr);
 	return (new_location);
 }
