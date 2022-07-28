91bff0daa27662b6e4ffd6ed752a75f2aaf0841f
Bislon Zulu <bislonzulu1@outlook.com>
2022-07-28 21:05:40 +0200

    task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
-		ptr[i] s1[i];
+		ptr[i], s1[i];
 	for (j = 0; j < num; j++)
 		ptr[i + j] = s2[j];
 	ptr[i + j] = '\0';
