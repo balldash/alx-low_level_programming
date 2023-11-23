7a4acea3a89d14541d0e5692b38279a12feaf32f
Bislon Zulu <bislonzulu1@outlook.com>
2023-11-23 22:47:37 +0200

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,28 @@
-A file
+#include "main.h"
+
+/**
+ * binary_to_uint - converts from binary to unsigned int
+ * @b: A pointer to the string of chars to converst
+ * Return: the converted resuls
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	singed int i = 0, n = 0;
+
+	if (!b)
+		return (0);
+
+	while (b[i] ! '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		n <<= 1;
+
+		if (b[i] & 1)
+			n += 1;
+		i += 1;
+	}
+	return (n);
+}
