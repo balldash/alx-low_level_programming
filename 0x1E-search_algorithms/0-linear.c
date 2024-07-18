33ee7bf2441d2e1879711dd0c4eb34516c5df021
Bislon Zulu <bislonzulu1@outlook.com>
2024-07-18 09:03:00 +0200

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,20 +7,18 @@
  * @value: the value to search for
  * Return: the first index where value is located
  */
-int linear_search(int *array, size_t size, int value) {
+int linear_search(int *array, size_t size, int value)
+{
 	size_t i;
-    if (array == NULL) {
-        return -1;
-    }
 
-    for (i = 0; i < size; i++) {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value) {
-            return i;
-        }
-    }
+	if (array == NULL)
+		return (-1);
 
-    return -1;
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
-
-
